bf6545ace3e41f333b3843d0d30146240157f557
vargrgrgr <wolf@cau.ac.kr>
2022-07-12 17:29:38 +0900

    mobx attached

--- RNVP/index.js
+++ RNVP/index.js
@@ -1,12 +1,13 @@
 /**
  * @format
  */
+
 import React from 'react';
 import { AppRegistry } from 'react-native';
 
 import App from './src/App';
 import {name as appName} from './app.json';
-
+if (!new class { x }().hasOwnProperty('x')) throw new Error('Transpiler is not configured correctly');
 
 const REGISTER_APP = () => (
       <App />
